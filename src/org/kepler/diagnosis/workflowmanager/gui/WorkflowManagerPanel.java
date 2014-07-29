8a289b60b0cf838dde5c7a308fd2e4646fcf2b6a
chenjun <cxd1098@gmail.com>
2014-07-29 18:02:30 +0800

    add workflow manager tabpane to the South East location of the diagnosis view

---
+++ src/org/kepler/diagnosis/workflowmanager/gui/WorkflowManagerPanel.java
@@ -0,0 +1,61 @@
+package org.kepler.diagnosis.workflowmanager.gui;
+
+import javax.swing.JPanel;
+
+import org.kepler.gui.TabPane;
+import org.kepler.gui.TabPaneFactory;
+
+import ptolemy.actor.gui.TableauFrame;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.NamedObj;
+
+public class WorkflowManagerPanel extends JPanel implements TabPane
+{
+	private TableauFrame _frame;
+	private String _title;
+	
+	public WorkflowManagerPanel(String title)
+	{
+		_title = title;
+	}
+	public void initializeTab() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTabName()
+	{
+		return _title;
+	}
+
+	@Override
+	public TableauFrame getParentFrame()
+	{
+		return _frame;
+	}
+
+	@Override
+	public void setParentFrame(TableauFrame parent)
+	{
+		_frame = parent;
+	}
+	
+	public static class Factory extends TabPaneFactory
+	{
+
+		public Factory(NamedObj container, String name)
+				throws IllegalActionException, NameDuplicationException
+		{
+			super(container, name);
+		}
+		
+		public TabPane createTabPane(TableauFrame parent)
+		{
+			WorkflowManagerPanel wmp = new WorkflowManagerPanel(this.getName());
+			return wmp;
+		}
+	}
+}
